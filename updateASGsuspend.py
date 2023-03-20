4baa11f5dd83b4a6c654bf6970cbbc6b0e696a73
saisree0414 <50710250+saisree0414@users.noreply.github.com>
2023-03-20 10:16:24 +0530

    Create updateASGsuspend.py
    adding file

---
+++ updateASGsuspend.py
@@ -0,0 +1,13 @@
+import boto3
+import os
+import time
+
+client = boto3.client('autoscaling')
+response = client.suspend_processes(
+    AutoScalingGroupName='test',
+    ScalingProcesses=[
+        'Launch','Terminate',
+    ],
+)
+
+print(response)
