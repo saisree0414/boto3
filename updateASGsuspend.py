95492d8d3a65eee456fdb054866ea13799299acb
saisree0414 <50710250+saisree0414@users.noreply.github.com>
2023-04-16 22:39:20 +0530

    Updated code

--- updateASGsuspend.py
+++ updateASGsuspend.py
@@ -2,6 +2,10 @@ import boto3
 import os
 import time
 
+Stack= input("enter stack name: ")
+ASG= $(AWS cloud formation describe-stack-resources --stack-name $Stack --query 'StackResources[?ResourceType==`AWS::AutoScaling::AutoScalingGroup`].PhysicalResourceId' --output text)
+print(ASG)
+
 client = boto3.client('autoscaling')
 response = client.suspend_processes(
     AutoScalingGroupName='test',
